ef8aa3febceb39d21d77259930bd3096ae70021d
WClark-Sp18 <35515442+WClark-Sp18@users.noreply.github.com>
2018-01-30 17:25:17 -0800

    Update
    init() makes a cone
    render() renders the cone

--- cone.js
+++ cone.js
@@ -1,4 +1,5 @@
 var gl = null;
+var cone = null;
 
 function init() {
     var canvas = document.getElementById( "webgl-canvas" );
@@ -11,12 +12,15 @@ function init() {
     }
 
     gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+    
+    cone = new Cone(1000);
 
     render();
 }
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
+    cone.render();
 }
 
 window.onload = init;
