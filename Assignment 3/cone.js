b90f95cc9210323a377493ceb9c7ce18b3545c4d
WClark-Sp18 <35515442+WClark-Sp18@users.noreply.github.com>
2018-01-30 18:25:36 -0800

    added cone.js

---
+++ Assignment 3/cone.js
@@ -0,0 +1,28 @@
+var gl = null;
+var cone = null;
+
+function init() {
+    var canvas = document.getElementById( "webgl-canvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+
+    if ( !gl ) {
+        alert("Unable to setup WebGL");
+        return;
+    }
+
+    gl.clearColor( 0.25, 0.45, 0.75, 1.0 );
+	
+    cone = new Cone( 100 );
+
+    render();
+}
+
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    cone.render();
+}
+
+
+
+window.onload = init;
