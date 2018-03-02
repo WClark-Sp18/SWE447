761991303e15a2ec5d209e581c266970aa1fc229
WClark-Sp18 <35515442+WClark-Sp18@users.noreply.github.com>
2018-03-02 07:58:06 -0800

    Create mars.js

---
+++ Mars/mars.js
@@ -0,0 +1,17 @@
+var canvas;
+var gl;
+var Mars : {
+    radius : 0.532,
+    distance : 1.52,
+    year : 1.88,
+    color : [ 1.0, 0.0, 0.0, 1.0 ]
+  },
+  var V;
+  var P;
+  var time = 0.0;
+  var timeDelta = 0.5
+  
+  //---------------------------------------------------
+  
+  funciton init(){
+  }
