a1de6cd65398adf6eeec56b23b7ead022402016f
WClark-Sp18 <35515442+WClark-Sp18@users.noreply.github.com>
2018-03-02 15:34:33 -0800

    Update cone.js

--- RotateCone/cone.js
+++ RotateCone/cone.js
@@ -31,4 +31,39 @@ function init(){
     alert("Unable to setup WebGL");
     return;
   }
+  
+  canvas.onmousedown = function handleMouseDown(event) {
+        mouseDown = true;
+        lastMouseX = event.clientX;
+        lastMouseY = event.clientY;
+    }
+  
+  document.onmouseup = function handleMouseUp(event) {
+        mouseDown = false;
+        if (stoprotating) dAngle = 0.0;
+        return;
+    }
+  
+  document.onmousemove = function handleMouseMove(event) {
+		if (!mouseDown) {
+		  if(stoprotating) dAngle = 0.0;
+		  return;
+		}
+		var newX = event.clientX;
+		var newY = event.clientY;
+
+		var deltaX = newX - lastMouseX;
+		var deltaY = newY - lastMouseY;
+		dAngle = degToRad(deltaX + deltaY) * Math.PI * 5;
+		lastMouseX = newX;
+		lastMouseY = newY;
+    }
+  
+  gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+  gl.enable(gl.DEPTH_TEST);
+
+  cone = new Cone(gl);
+  resize();
+	
+  window.requestAnimationFrame(render);
 }
