da64238a3c811c1157bcd4b3d167642e10bc1f35
WClark-Sp18 <35515442+WClark-Sp18@users.noreply.github.com>
2018-03-02 15:13:17 -0800

    Update cone.js

--- RotateCone/cone.js
+++ RotateCone/cone.js
@@ -1,3 +1,27 @@
 var cone;
 var canvas;
 var gl;
+
+var near = 10.0;
+var far = 100.0;
+
+var M;
+var V;
+
+var angle;
+var dAngle;
+
+var mouseDown = false;
+var lastMouseX = null;
+var lastMouseY = null;
+
+var zvalue = -0.5*(near + far);
+var offset = [ 0.0,  0.0, 0.0 ];
+var rotationAxis = undefined;
+var xAxis = [1, 0, 0];
+var yAxis = [0, 1, 0];
+var speed = 1;
+var stoprotating = 0;
+
+function init(){
+}
