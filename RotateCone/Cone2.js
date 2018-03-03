d5c2a7483f31d1f02b5f1cb8e91cfa01aefce1b8
WClark-Sp18 <35515442+WClark-Sp18@users.noreply.github.com>
2018-03-02 17:59:46 -0800

    Update Cone2.js

--- RotateCone/Cone2.js
+++ RotateCone/Cone2.js
@@ -1,4 +1,4 @@
-const DefaultNumSides = 8;
+const nSides = 8;
 
 function Cone( gl, numSides, vertexShaderId, fragmentShaderId ) {
     var vertShdr = vertexShaderId || "Cone-vertex-shader";
@@ -13,16 +13,15 @@ function Cone( gl, numSides, vertexShaderId, fragmentShaderId ) {
         return; 
     }
 
-    var n = numSides || DefaultNumSides; 
+    var n = numSides || nSides; 
 
     var theta = 0.0;
-    var dTheta = 2.0 * Math.PI / n;
-    
-    this.positions = { numComponents : 3 };
-	  this.colors = {numComponents : 3 };
-    
+    var dTheta = 2.0 * Math.PI / n;	
+    this.positions = { numComponents : 3 };t
+    his.colors = {numComponents : 3 };
+	var colors = [1.0, 0.0, 1.0];
     var positions = [ 0.0, 0.0, 0.0 ];
-	  var colors = [1.0, 0.0, 0.0];
+	
     var indices = [ 0 ];
     
     for ( var i = 0; i < n; ++i ) {
@@ -34,7 +33,7 @@ function Cone( gl, numSides, vertexShaderId, fragmentShaderId ) {
     }
 
     positions.push( 0.0, 0.0, 1.0 );
-	  colors.push(0.0, 0.0, 1.0);
+	  colors.push(0.0, 1.0, 1.0);
     
     indices.push(n);
 
@@ -48,9 +47,9 @@ function Cone( gl, numSides, vertexShaderId, fragmentShaderId ) {
     gl.bindBuffer( gl.ARRAY_BUFFER, this.positions.buffer );
     gl.bufferData( gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW );
 	
-	  this.colors.buffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, this.colors.buffer );
-	  gl.bufferData( gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW );
+	this.colors.buffer = gl.createBuffer();
+	gl.bindBuffer( gl.ARRAY_BUFFER, this.colors.buffer );
+	gl.bufferData( gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW );
 
     this.indices.buffer = gl.createBuffer();
     gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer );
@@ -59,49 +58,48 @@ function Cone( gl, numSides, vertexShaderId, fragmentShaderId ) {
     this.positions.attributeLoc = gl.getAttribLocation( this.program, "vPosition" );
     gl.enableVertexAttribArray( this.positions.attributeLoc );
 	
-	  this.colors.attributeLoc = gl.getAttribLocation( this.program, "vColor" );
-    gl.enableVertexAttribArray( this.colors.attributeLoc );
+	this.colors.attributeLoc = gl.getAttribLocation( this.program, "vColor" );
+	gl.enableVertexAttribArray( this.colors.attributeLoc );
 	
 	  this.uniforms = {
 	    MV : undefined,
 	    P : undefined
 	  };
 
-	  this.uniforms.MV = gl.getUniformLocation(this.program, "MV");
-	  this.uniforms.P = gl.getUniformLocation(this.program, "P");
-
-  	this.MV = mat4(); // or undefined
+	this.uniforms.MV = gl.getUniformLocation(this.program, "MV");
+	this.uniforms.P = gl.getUniformLocation(this.program, "P");
+	this.MV = mat4(); // or undefined
   	this.P = mat4();
 
     this.render = function () {
-        gl.useProgram( this.program );
+	    gl.useProgram( this.program );
 
-        gl.bindBuffer( gl.ARRAY_BUFFER, this.positions.buffer );
-        gl.vertexAttribPointer( 
-			      this.positions.attributeLoc, 
-			      this.positions.numComponents,
-            gl.FLOAT, 
-			      gl.FALSE, 
-			      3 * Float32Array.BYTES_PER_ELEMENT, 
-			      0 );
-		
-		gl.bindBuffer( gl.ARRAY_BUFFER, this.colors.buffer );
-    gl.vertexAttribPointer( 
-			      this.colors.attributeLoc, 
-			      this.colors.numComponents,
-            gl.FLOAT, 
-			      gl.FALSE, 
-			      3 * Float32Array.BYTES_PER_ELEMENT, 
-			      0);
+	    gl.bindBuffer( gl.ARRAY_BUFFER, this.positions.buffer );
+	    gl.vertexAttribPointer( 
+			      	this.positions.attributeLoc, 
+			      	this.positions.numComponents,
+            			gl.FLOAT, 
+			      	gl.FALSE, 
+			      	3 * Float32Array.BYTES_PER_ELEMENT, 
+			      	0 );
+	    
+	    gl.bindBuffer( gl.ARRAY_BUFFER, this.colors.buffer );
+	    gl.vertexAttribPointer( 
+			      	this.colors.attributeLoc, 
+			      	this.colors.numComponents,
+	    			gl.FLOAT, 
+			      	gl.FALSE, 
+			      	3 * Float32Array.BYTES_PER_ELEMENT, 
+			      	0);
  
-    gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer );
+	    gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer );
 				
-		gl.uniformMatrix4fv( this.uniforms.MV, gl.FALSE, flatten(this.MV) );
-		gl.uniformMatrix4fv( this.uniforms.P, gl.FALSE, flatten(this.P) );
+	    gl.uniformMatrix4fv( this.uniforms.MV, gl.FALSE, flatten(this.MV) );
+	    gl.uniformMatrix4fv( this.uniforms.P, gl.FALSE, flatten(this.P) );
 		
-    gl.drawElements( gl.TRIANGLE_FAN, this.indices.count, gl.UNSIGNED_SHORT, 0 );
+	    gl.drawElements( gl.TRIANGLE_FAN, this.indices.count, gl.UNSIGNED_SHORT, 0 );
 		
-    var offset = this.indices.count * 2 
-    gl.drawElements( gl.TRIANGLE_FAN, this.indices.count, gl.UNSIGNED_SHORT, offset );
+	    var offset = this.indices.count * 2 
+	    gl.drawElements( gl.TRIANGLE_FAN, this.indices.count, gl.UNSIGNED_SHORT, offset );
     }
 };
